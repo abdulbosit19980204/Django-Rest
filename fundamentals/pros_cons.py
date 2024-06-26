2a1298c9bf3c6ceaf80881ecdaf7f55c2264f3ac
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-06-26 09:16:21 +0500

    Fundamentals

---
+++ fundamentals/pros_cons.py
@@ -0,0 +1,8 @@
+# There are many pros and cons of Rest
+
+# 1. Authentication policies including packages for OAuth1a and OAuth2
+# 2. Serialization that supports both ORM and non-ORM data source
+# 3. Customizable (Moslashuvchan)
+
+
+# Requirements
